874ae6476d92341413852b4fe820f491b5929944
Alan Fung <alankjfung@gmail.com>
2026-02-20 02:23:13 -0800

    i made a window!

--- main.c
+++ main.c
@@ -1,5 +1,23 @@
+#include "SDL3/SDL_video.h"
 #include <stdio.h>
+#include <SDL3/SDL.h>
 
 int main() {
-    printf("hello world\n");
+    SDL_Window *win = SDL_CreateWindow(
+        "Hello World!", // title
+        100, // width
+        100, // height
+        SDL_WINDOW_FULLSCREEN // flags
+    );
+
+    if (win == NULL) {
+        printf("sdl window creation error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Delay(10000);
+
+    SDL_DestroyWindow(win);
+    SDL_Quit();
 }
